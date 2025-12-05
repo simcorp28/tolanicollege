d0ab40ea9e5c299449b6d85248cd64fcb2cb9b4e
Gaurav <your-email@example.com>
2025-12-06 00:07:16 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-yellow-200 bg-black p-4'>
+        WE ARE WINNING
+      </div>
+    </>
+  )
+}
+
+export default App
+
